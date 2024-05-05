1aab6a766433e8e8b9b4f863cee7a4ab5645e5a7
Damian Capdevila <damian.capdevila@hotmail.com>
2024-05-05 11:53:46 +0200

    add authentication and authorization

---
+++ Services/IRecruiterService.cs
@@ -0,0 +1,13 @@
+﻿using HelloRecruiter.Models;
+
+namespace HelloRecruiter.Services
+{
+	public interface IRecruiterService
+	{
+		public List<Recruiter> List();
+		public Recruiter Get(int id);
+		public Recruiter Create(Recruiter recruiter);
+		public Recruiter Update(Recruiter recruiter);
+		public bool Delete(int id);
+	}
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using HelloRecruiter.Models;
+using HelloRecruiter.Repositories;
+
+namespace HelloRecruiter.Services
+{
+	public class UserService : IUserService
+	{
+		public User Get(UserLogin userLogin)
+		{
+				User user = UserRepository.Users.FirstOrDefault(user => user.Username.Equals(userLogin.Username, StringComparison.OrdinalIgnoreCase) &&
+															    user.Password.Equals(userLogin.Password));
+				return user;
+		}
+	}
+}
